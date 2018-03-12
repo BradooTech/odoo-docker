193fb8b69208ff42f3e3a90c898ab66f441a0c2b
Gabriel Balog <gabriel.balog@bradootech.com>
2018-03-12 16:11:59 -0300

    Adicionado script de clone

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone git@github.com:odoo/enterprise --depth=1
 git clone git@github.com:Trust-Code/odoo-brasil --depth=1
-git clone git@github.com:bradoodev/marketplace --depth=1
+git clone git@github.com:bradoodev/marketplace addons/odoo_marketplace --depth=1
