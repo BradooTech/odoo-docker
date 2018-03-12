b9c93044315f681e2d4c74910fe43a75307f5134
Gabriel Balog <gabriel.balog@bradootech.com>
2018-03-12 17:32:57 -0300

    Alteracoes para Openeducat

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone git@github.com:odoo/enterprise --depth=1
 git clone git@github.com:Trust-Code/odoo-brasil --depth=1
-git clone git@github.com:bradoodev/marketplace addons/odoo_marketplace --depth=1
+git clone git@github.com:bradoodev/openeducat/openeducat_erp openeducat_erp --depth=1
 git clone git@github.com:odoo/odoo --depth=1
 cp -r ./odoo/addons ./odoo/odoo
