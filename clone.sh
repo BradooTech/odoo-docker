94e9aac81bf472b68b003bc451392141518356df
Gabriel Balog <gabriel.balog@bradootech.com>
2018-03-13 10:19:02 -0300

    Adicionando branch para enteprise

--- clone.sh
+++ clone.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone git@github.com:odoo/enterprise --depth=1
 git clone git@github.com:Trust-Code/odoo-brasil --depth=1
-git clone git@github.com:openeducat/openeducat_erp.git openeducat_erp --depth=1
 git clone git@github.com:odoo/odoo --depth=1
 cp -r ./odoo/addons ./odoo/odoo
