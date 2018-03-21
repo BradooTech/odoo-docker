d5d1a522f8fccd8c91cc7cdafb6e33685351cbb5
Gabriel Balog <gabriel.balog@bradootech.com>
2018-03-21 11:27:08 -0300

    Alterando clone para https e adicionando opcao de restart no db

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone git@github.com:odoo/enterprise --depth=1
-git clone git@github.com:Trust-Code/odoo-brasil --depth=1
-git clone git@github.com:odoo/odoo --depth=1
+git clone https://github.com/odoo/enterprise --depth=1
+git clone https://github.com/Trust-Code/odoo-brasil --depth=1
+git clone https://github.com/odoo/odoo --depth=1
 cp -r ./odoo/addons ./odoo/odoo
