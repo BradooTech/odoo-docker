ca8db3b45cd814ac20e785665db83975f9a66efa
Gabrie Balog <gabriel.balog@bradootech.com>
2018-03-27 13:12:44 -0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git clone https://github.com/Trust-Code/odoo-brasil --depth=1
 git clone https://github.com/odoo/odoo --depth=1
-git clone https://github.com/bradoodev/tecnospeed --depth=1
 cp -r ./odoo/addons ./odoo/odoo
