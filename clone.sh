0ad5aea36f6f5d12b16dc992442e02f4dd88f4e9
Gabriel Balog <gabriel.balog@bradootech.com>
2018-03-28 10:10:42 -0300

    Alterando o repositorio do arquivo de clone para o da bradootech

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/odoo/enterprise --depth=1
-git clone https://github.com/Trust-Code/odoo-brasil --depth=1
+git clone https://github.com/bradootech/odoo-brasil --depth=1
 git clone https://github.com/odoo/odoo --depth=1
 cp -r ./odoo/addons ./odoo/odoo
