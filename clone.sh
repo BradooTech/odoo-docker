142b80c7b4708a945f5e8c682461c5792cb2d40c
Gabrie Balog <gabriel.balog@bradootech.com>
2018-10-08 16:47:30 -0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/BradooTech/odoo-brasil.git --depth=1
-git clone https://github.com/odoo/odoo.git --depth=1
+git clone https://github.com/BradooTech/odoo-brasil.git -b 11.0 --depth=1
+git clone https://github.com/odoo/odoo.git -b 11.0 --depth=1
 cp -r ./odoo/addons ./odoo/odoo
